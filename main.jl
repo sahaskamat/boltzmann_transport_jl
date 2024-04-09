7a2b99a4bc6a14c9e5b760b4c0430f1c05131143
sahaskamat <sahaskamat@yahoo.co.in>
2024-04-09 17:51:50 -0400

    added orbits diagnostics to main

--- main.jl
+++ main.jl
@@ -4,14 +4,17 @@ using Plots
 
 include("dispersion.jl")
 include("createinitialpoints.jl")
+include("createorbits.jl")
 
 B=[45*sin(pi/4),0,45*cos(pi/4)]  #a field of 45T along the z axis
+
 
 disp = make_NdLSCO_dispersion(160f-3,-0.1364,0.0682,0.0651,0.8243,true)
 initialpoints = createinitialpoints(disp["c_eff"],disp["E"],50)
 extended_intialpoints = extendedzonemultiply(initialpoints,2,disp["c_eff"])
 interpolatedcurves = interpolate3d(initialpoints,disp["c_eff"])
 intersectionpoints = makeorbitpoints(extended_intialpoints,interpolatedcurves,B,5,disp["c_eff"])
+orbits = createorbits(intersectionpoints,B,disp["gradE"])
 
 #diagnostics, this plots a x-z plane projection of calculated initial points
 plot([0],[0],[0])
@@ -35,4 +38,11 @@ for set_of_intersections_with_a_plane in intersectionpoints
     scatter!(matrix_of_points[1,:],matrix_of_points[2,:],matrix_of_points[3,:])
 end
 
+#diagnostics to make sure orbits are being made correctly
+for orbits_in_plane in orbits
+    for curve in orbits_in_plane
+        matrix_of_points = hcat(curve...)
+        plot!(matrix_of_points[1,:],matrix_of_points[2,:],matrix_of_points[3,:])
+    end
+end
 savefig("interpolatedpoints.png")
